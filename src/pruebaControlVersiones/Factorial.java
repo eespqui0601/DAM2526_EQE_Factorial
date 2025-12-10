44944f3021cd7ed01da5cb798ba3c1989ae32084
eespqui0601 <eespqui0601@g.educaand.es>
2025-12-10 08:52:04 +0100

    Versión F1

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -47,5 +47,12 @@ public class Factorial {
 	        		return factorial;
 	        	}
 	        }
+        private static long factorialRec(int n) {
+            if (n == 0 || n == 1) {
+              return 1;
+            }
+
+            return n * factorialRec(n - 1);
         }
+}
 
