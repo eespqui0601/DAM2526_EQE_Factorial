a687c942285792c11edd0055bc475bbe670d0f31
eespqui0601 <eespqui0601@g.educaand.es>
2025-12-10 08:45:09 +0100

    Segunda versión

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Factorial {
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Introduce un número entero mayor o igual que 0: ");
-        int num = sc.nextInt();
+        
+		int num = pedirNumero();
 
         if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
         } else {
             System.out.println("Has introducido el número: " + num);
         }
-
         sc.close();
 	}
+	private static Scanner sc = new Scanner(System.in);
+        private static int pedirNumero() {
+        	 System.out.print("Introduce un número entero mayor o igual que 0: ");
+     	    return sc.nextInt();
+        }
 
 }
