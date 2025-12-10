c385b10a763557fb5f1e98f408dbf58da950375c
eespqui0601 <eespqui0601@g.educaand.es>
2025-12-10 09:26:38 +0100

    Versión F

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -14,6 +14,7 @@ public class Factorial {
             System.out.println("Has introducido el número: " + num);
         }
         System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
+        System.out.println("RESULTADO - El factorial del número " + num + " es " + factorialBucle(num));
         sc.close();
 	}
 	private static Scanner sc = new Scanner(System.in);
